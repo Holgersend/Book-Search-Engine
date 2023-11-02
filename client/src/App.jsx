b73024b8e8838713b82b6a437abdbef8a27b32a1
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-11-01 23:38:53 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,36 @@ import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+// Import ApolloProvider and ApolloClient
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// client instance of ApolloClient 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <>
